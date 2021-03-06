84fc9892fce113a92441ef34345bd71c884e1a62
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-03-05 23:59:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CSV Download React
         </p>
         <DownloadLink
           label="Download"
